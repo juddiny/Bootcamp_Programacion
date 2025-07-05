91b38aae344ca12fa3d39633799b3934b16c06ce
juddiny <juan.toro60569@ucaldas.edu.co>
2025-07-05 10:31:03 -0500

    Clase 05072025 entrada-procesos-salidad

---
+++ ENTRADAS-PROCESOS-SALIDAS/areaTriangulo.py
@@ -0,0 +1,16 @@
+#1. inicializacion de variables y reservar espacios de memoria
+base=0
+altura=0
+area=0
+
+#2. Entrada por teclado con imput
+base= float(input("BASE : "))
+altura= float(input("ALTURA : "))
+
+#3. procesos
+
+area= base * altura /2
+
+#4. salida con print
+
+print(area)
---
+++ variablesTiposDatos.py
@@ -0,0 +1,18 @@
+"""
+Comentario
+multilinea
+"""
+#comentario de una sola linea
+
+nombre="Juan Toro"
+
+print(nombre)
+print("Nombre de tipo: ",type(nombre))
+
+
+#variables de tipo entero
+
+
+#variables de tipo decimal
+
+
